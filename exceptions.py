10ee3a5d8fb373e8be19cb6e3214edcbbdbd860d
Oleksandii <alexeyspace3@gmail.com>
2023-05-05 19:17:19 +0300

    Adding Exceptions.py

---
+++ exceptions.py
@@ -0,0 +1,43 @@
+try:
+    x = int(input('Write a number '))
+    print(f'x is {x}')
+except ValueError:
+    print('It is not a number')
+########
+try:
+    x = int(input('Write a number '))
+    
+except ValueError:
+    print('It is not a number')
+else:
+    print(f'x is {x}')
+################
+try:
+    x = int(input('Write a number '))
+    
+except ValueError:
+    print('It is not a number')
+
+print(f'x is {x}')
+##################
+try:
+    x = int(input('Write a number '))   # same 3 last 
+    
+except ValueError:
+    print('It is not a number')
+else:
+    return x
+############
+try:
+    x = int(input('Write a number '))
+    return x
+except ValueError:
+    print('It is not a number')
+
+    
+####################
+try:
+    return int(input('Write a number '))
+    
+except ValueError:
+    print('It is not a number')
