ac761e3d218fdccecca45132afcfea32a2c3d32e
Oleksandii <alexeyspace3@gmail.com>
2023-05-08 11:11:39 +0300

    Adding unittests

---
+++ Python/unittests.py
@@ -0,0 +1,27 @@
+
+
+
+    
+def square(number=1):
+    return number * number
+
+
+def hello(to='world'):
+    return(f'Hello {to}')
+
+
+
+
+
+def main():
+    print(square(3))
+    print(square(5))
+    name= input('What is your name?')
+    hello(name)
+
+
+
+
+
+if __name__ =='__main__':
+    main()
---
+++ Python/unittestspy.py
@@ -0,0 +1,46 @@
+import pytest
+from unittests import square , hello
+
+
+def test_hello():
+    assert hello('David')=='Hello David'
+
+def test_positive():
+    assert square(8)== 64
+    assert square(10)== 100
+def test_negative():
+    assert square(-5)== 25
+    assert square(-3)== 9
+def test_zero():
+    assert square(0)== 0
+def test_str():
+    with pytest.raises(TypeError):
+        square('cat')
+
+
+
+
+
+'''
+############ pytest retriew only 1 problem (running by pytest <name of file> in dirictory)
+def testbypytest():
+    assert square(8)== 64
+    assert square(10)== 100
+    assert square(9)== 81
+    assert square(-1)== 1
+    assert square(0)== 0
+
+
+######## Dumb method with a lot of code 
+
+def test():
+    try:
+        assert square(8)== 64
+        
+    except AssertionError:
+        print('Square function working not properly 8*8!=64')
+    try:
+        assert square(10) == 100
+    except AssertionError:
+        print('Square function working not properly 10*10 !=100')
+'''
